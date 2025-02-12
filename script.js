2722464bc5f9c9de7bbc355d1efee903994a39f3
rickdevcod <tribulus22214@gmail.com>
2025-02-12 13:20:19 -0300

    Atualização 1.1.3

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function toggleMenu() {
+document.addEventListener("DOMContentLoaded", () => {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+
+  if (menu && icon) {
+    icon.addEventListener("click", () => {
+      menu.classList.toggle("open");
+      icon.classList.toggle("open");
+    });
+  }
+});
